584e1dc5115466695eb51f37571089d7a3f05075
ser-hub <serkanofficial@abv.bg>
2024-02-17 14:44:07 +0200

    Add ErrorMessages Service

---
+++ src/Utilities/ErrorMessages.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Utilities;
+
+class ErrorMessages
+{
+    const OBJECT_NOT_FOUND_MESSAGE = 'Object not found.';
+    const ACCESS_TO_RESOURCE_DENIED_MESSAGE = 'This resource belongs to another user.';
+}
